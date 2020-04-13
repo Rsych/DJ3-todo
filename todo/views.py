6cb568cf2b211051294c50a79a18331de3813313
Ryan J.W Kim <tamielryan@gmail.com>
2020-04-13 22:43:24 +0300

    creating user

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def signupuser(request):
-    
-    return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        # create new user
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+        #pass not match
+            print("helo")
